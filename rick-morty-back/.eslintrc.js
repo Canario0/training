8a6437be112ddb0fa763074c8b376c2d905a167c
Canario0 <prenero4bc@gmail.com>
2022-04-27 17:48:11 +0200

    ci: added basic structure and eslint config

---
+++ rick-morty-back/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    quotes: ["error", "double"],
+    "import/extensions": ["error", "never"],
+    "prettier/prettier": ["error"],
+  },
+};
