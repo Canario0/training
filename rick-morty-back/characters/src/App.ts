be29bf065ff1e9fd89d7407c91cd7c59b26954ec
Canario0 <prenero4bc@gmail.com>
2022-05-12 11:31:29 +0200

    ref: remove user from monolith

--- rick-morty-back/characters/src/App.ts
+++ rick-morty-back/characters/src/App.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { setupMiddleware, setUpErrorHandlers } from "./middleware";
 import characterRouter from "./characters/character-routes";
-import userRouter from "./users/user-routes";
 import setUpDatabase from "./database";
 
 const app = express();
@@ -10,7 +9,6 @@ setUpDatabase();
 setupMiddleware(app);
 
 routes.use("/characters", characterRouter);
-routes.use("/users", userRouter);
 app.use("/v1", routes);
 
 setUpErrorHandlers(app);
--- rick-morty-back/characters/src/database.ts
+++ rick-morty-back/characters/src/database.ts
@@ -2,9 +2,7 @@ import { DataSource } from "typeorm";
 import { TypeCharacter } from "./characters/entities/character-entity";
 import { TypeLocation } from "./characters/entities/location-entity";
 import { TypeOrigin } from "./characters/entities/origin-entity";
-import { TypeUser } from "./users/user-entity";
 import characters from "./characters/character-demo";
-import users from "./users/user-demo";
 
 export const dataSource = new DataSource({
   type: "postgres",
@@ -15,7 +13,7 @@ export const dataSource = new DataSource({
   database: "characters",
   synchronize: true,
   logging: false,
-  entities: [TypeOrigin, TypeLocation, TypeCharacter, TypeUser],
+  entities: [TypeOrigin, TypeLocation, TypeCharacter],
   migrations: [],
   subscribers: [],
 });
@@ -24,6 +22,5 @@ export default function setUpDatabase() {
   dataSource.initialize().then(() => {
     // TODO: extract this part into a initialization script.
     dataSource.getRepository(TypeCharacter).save(characters);
-    dataSource.getRepository(TypeUser).save(users);
   });
 }
